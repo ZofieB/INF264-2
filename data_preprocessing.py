abee5f94dc14a55303c9e14dad0fb307a362ced1
sophie <sophie.m.blum@gmail.com>
2020-10-03 16:38:47 +0200

    Feature engineering done with resulting feature matrix

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,12 +5,12 @@ import datetime
 
 #read file to input
 df = pd.read_csv('data.csv', header = None)
-X = df.to_numpy()
-X = np.array([[int(value) for value in array_element] for array_element in X[1:]])
-fra_time = X[:, 3]
-volume_til_sntr = X[:, 4]
-volume_til_dnp = X[:, 5]
-volume_totalt = X[:, 6]
+X_raw = df.to_numpy()
+X_raw = np.array([[int(value) for value in array_element] for array_element in X_raw[1:]])
+fra_time = X_raw[:, 3]
+volume_til_sntr = X_raw[:, 4]
+volume_til_dnp = X_raw[:, 5]
+volume_totalt = X_raw[:, 6]
 
 #plot datapoints per hour for both directions
 fig = plt.figure(1, figsize=(10,10))
@@ -34,12 +34,26 @@ sat = []
 sun = []
 weekdays = [mon, tue, wed, thu, fri, sat, sun]
 dates = []
-for datapoint in X:
+jan = []
+feb = []
+mar = []
+apr = []
+mai = []
+jun = []
+jul = []
+aug = []
+sep = []
+octo = []
+nov = []
+dec = []
+months = [jan, feb, mar, apr, mai, jun, jul, aug, sep, octo, nov, dec]
+for datapoint in X_raw:
     datetime_point = datetime.datetime(datapoint[0], datapoint[1], datapoint[2], hour = datapoint[3])
     new_datapoint = [datetime_point, datapoint[4], datapoint[5], datapoint[6]]
     dates.append(new_datapoint)
-    day_index = datetime_point.weekday()
-    weekdays[day_index].append(new_datapoint)
+    day_indeX_raw = datetime_point.weekday()
+    weekdays[day_indeX_raw].append(new_datapoint)
+    months[datetime_point.month - 1].append(new_datapoint)
 
 #plot every weekday seperately
 # Display scatterplots of target prices with respect to each of the 13 features:
@@ -67,14 +81,105 @@ for day in range(len(weekdays)):
     vol_til_dnp_temp = weekday_points_np[:, 2]
     plt.figure(2, figsize=(16, 10))
     if day < len(weekdays) - 1:
-        plot_idx = day+1
+        plot_idX_raw = day+1
     else:
-        plot_idx = 7
-    plt.subplot(4, 2, plot_idx)
+        plot_idX_raw = 7
+    plt.subplot(4, 2, plot_idX_raw)
     plt.scatter(fra_time_temp,vol_til_sntr_temp, label="Volume to centre", c = colors[day])
     plt.scatter(fra_time_mod_temp,vol_til_dnp_temp, label="Volume to Danmarksplass", c = colors[day + 7])
-    plt.xlabel(weekday_strings[day])
+    plt.X_rawlabel(weekday_strings[day])
     plt.ylabel('Volume')
     plt.legend()
 plt.suptitle("Volume in both directions for each weekday")
+
+month_strings = ["January", "Feburary", "March", "April", "Mai", "June", "July", "August", "September", "October", "November", "December"]
+
+for month in range(len(months)):
+    month_points_np = np.array(months[month])
+    datetime_arr = month_points_np[:, 0]
+    fra_time_temp = [date.hour for date in datetime_arr]
+    fra_time_mod_temp = np.array(fra_time_temp) + 0.4
+    vol_til_sntr_temp = month_points_np[:, 1]
+    vol_til_dnp_temp = month_points_np[:, 2]
+    plt.figure(3, figsize=(16, 10))
+    if month < len(months) - 1:
+        plot_idX_raw = month + 1
+    else:
+        plot_idX_raw = 12
+    plt.subplot(3, 4, plot_idX_raw)
+    plt.scatter(fra_time_temp,vol_til_sntr_temp, label="Volume to centre", c = colors[0])
+    plt.scatter(fra_time_mod_temp,vol_til_dnp_temp, label="Volume to Danmarksplass", c = colors[1])
+    plt.X_rawlabel(month_strings[month])
+    plt.ylabel('Volume')
+    plt.legend()
+plt.suptitle("Volume in both directions for each month")
 plt.show()
+
+#creating the feature matriX_raw:
+#0 weekday 1 saturday 2 sunday/free day 3 hours continuous feature
+fe_datapoints = []
+Y_dnp_list = []
+Y_sntr_list = []
+Y_totalt_list = []
+for datapoint in dates:
+    day_indeX_raw = datapoint[0].weekday()
+    # Christmas
+    if datapoint[0].month == 12 and (datapoint[0].day == 25 or datapoint[0].day == 26):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #New Years
+    elif datapoint[0].month == 1 and datapoint[0].day == 1:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #Constitution day, itnernational workers day
+    elif datapoint[0].month == 5 and (datapoint[0].day == 17 or datapoint[0].day == 1):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #all the easters
+    elif datapoint[0].year == 2015 and datapoint[0].month == 4 and (datapoint[0].day == 2 or datapoint[0].day == 3 or datapoint[0].day == 6):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2016 and datapoint[0].month == 3 and (datapoint[0].day == 24 or datapoint[0].day == 25 or datapoint[0].day == 28):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2017 and datapoint[0].month == 4 and (datapoint[0].day == 13 or datapoint[0].day == 14 or datapoint[0].day == 17):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif (datapoint[0].year == 2018 and datapoint[0].month == 4 and datapoint[0].day == 2) or (datapoint[0].month == 2 and (datapoint[0].day == 30 or datapoint[0].day == 29)):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2019 and datapoint[0].month == 4 and (datapoint[0].day == 18 or datapoint[0].day == 19 or datapoint[0].day == 22):
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #Ascention days
+    elif datapoint[0].year == 2015 and datapoint[0].month == 5 and datapoint[0].day == 14:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2016 and datapoint[0].month == 5 and datapoint[0].day == 5:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2017 and datapoint[0].month == 5 and datapoint[0].day == 25:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2018 and datapoint[0].month == 5 and datapoint[0].day == 10:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2019 and datapoint[0].month == 5 and datapoint[0].day == 30:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #Whit mondays
+    elif datapoint[0].year == 2015 and datapoint[0].month == 5 and datapoint[0].day == 25:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2016 and datapoint[0].month == 5 and datapoint[0].day == 16:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2017 and datapoint[0].month == 6 and datapoint[0].day == 5:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2018 and datapoint[0].month == 5 and datapoint[0].day == 21:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    elif datapoint[0].year == 2019 and datapoint[0].month == 6 and datapoint[0].day == 10:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #saturdays
+    elif day_indeX_raw == 5:
+        fe_datapoints.append([0,1,0,datapoint[0].hour])
+    #sundays
+    elif day_indeX_raw == 6:
+        fe_datapoints.append([0,0,1,datapoint[0].hour])
+    #normal weekdays
+    else:
+        fe_datapoints.append([1,0,0,datapoint[0].hour])
+    #append label values
+    Y_sntr_list.append(datapoint[1])
+    Y_dnp_list.append(datapoint[2])
+    Y_totalt_list.append(datapoint[3])
+
+X = np.array(fe_datapoints)
+Y_sntr = np.array(Y_sntr_list)
+Y_dnp = np.array(Y_dnp_list)
+Y_totalt = np.array(Y_totalt_list)
